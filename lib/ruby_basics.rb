40855ef292d48ccaf7d7e27739b0dbf553aefd33
Miguel Henriquez <henriquez.miguel@outlook.com>
2019-02-12 20:13:54 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,16 +1,16 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+  "#{value}"
 end
 
-def argue
-  
+def argue(phrase)
+  "#{phrase}"
 end
 
-def greeting
+def greeting(greeting,name)
 
 end
 
